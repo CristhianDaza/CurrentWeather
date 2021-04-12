9ccfee9bedc77991f031ee225fd4899e0ae09246
Cristhian Daza <cristian.d.b@hotmail.com>
2021-04-12 18:58:41 -0500

    Llamando a la api

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,32 @@
 </template>
 
 <script>
-
 export default {
   name: "App",
-  components: {
+  components: {},
+  methods: {
+    currentApi() {
+      const api = "ffd452f7ae40a393bc21cd201b41cc87";
+      const city = "Bogota";
+
+      const xhr = new XMLHttpRequest();
+
+      xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+          console.log(this.responseText);
+        }
+      });
+
+      xhr.open(
+        "GET",
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`
+      );
+
+      xhr.send();
+    },
+  },
+  mounted() {
+    this.currentApi();
   },
 };
 </script>
