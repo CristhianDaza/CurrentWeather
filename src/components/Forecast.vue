854249b0e52b33c73a18b0af7f596b2fd40bda46
Cristhian Daza <cristian.d.b@hotmail.com>
2021-04-13 15:59:27 -0500

    Creando componente de visit

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="container">
         <ForecastDay />
-        <div class="visit"></div>
+        <Visit />
         <div class="top"></div>
         <div class="locations"></div>
     </div>
 </template>
 
 <script>
-import ForecastDay from "@/components/ForecastDay.vue"
+import ForecastDay from "@/components/ForecastDay.vue";
+import Visit from "@/components/Visit.vue";
+
 export default {
     components: {
         ForecastDay,
+        Visit,
     }
 }
 </script>
@@ -19,5 +22,6 @@ export default {
 <style scoped>
     .container {
         display: flex;
+        justify-content: space-around;
     }
 </style>
---
+++ src/components/Visit.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <h2>Place to <span>Visit</span></h2>
+    <div class="containerimg">
+      <img class="visitimg" src="/arab-street.jpg" alt="Arab Street" />
+      <div class="infoCity">
+        <div class="iconCity">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            role="img"
+            aria-hidden="true"
+            class="icon"
+          >
+            <path :d="mdiMapMarker"></path>
+          </svg>
+        </div>
+        <div class="nameCity">
+          <p>Arab Street</p>
+          <p>Singapore</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mdiMapMarker } from "@mdi/js";
+
+export default {
+  data() {
+    return {
+      mdiMapMarker,
+    };
+  },
+};
+</script>
+
+
+<style>
+.containerimg {
+  height: 250px;
+  width: 250px;
+  margin-top: 40px;
+
+  position: relative;
+}
+.visitimg {
+  max-width: 100%;
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+  border-radius: 25px;
+}
+.containerimg .infoCity {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  color: white;
+  font-weight: 800;
+  font-size: 12px;
+}
+
+.iconCity svg {
+  fill: currentColor;
+  vertical-align: top;
+  width: 20px;
+  display: inline-block;
+  margin-right: 5px;
+}
+
+.infoCity {
+  display: flex;
+}
+</style>
